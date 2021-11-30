91ca6c0894158b1c89e603eedbc7ac3e816d3031
sidhant bahri <sidhantbahri01@gmail.com>
2021-11-30 22:41:03 +0530

    new changes

--- server.js
+++ server.js
@@ -43,6 +43,6 @@ paytmParams['INDUSTRY_TYPE_ID'] = process.env.PAYTM_INDUSTRY_TYPE_ID,
 paytmParams['ORDER_ID'] = uuid(),
 paytmParams['CUST_ID'] = process.env.PAYTM_CUST_ID,
 paytmParams['TXN_AMOUNT'] = '100',
-paytmParams['CALLBACK_URL'] = 'https://topdealz.herokuapp.com/callback'
+paytmParams['CALLBACK_URL'] = '/callback'
 paytmParams['EMAIL'] = 'sidhantbahri01@gmail.com'
 paytmParams['MOBILE_NO'] = '1234567852'
